bad13093388a8c6b4e5c66f8139c2e40941181f8
Thomas Szentirmay <tlszent@outlook.com>
2023-08-21 18:19:07 +1000

    styled nav links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,15 @@ const Header = () => {
     return(
         <div className="header">
 
-            <img src={Logo} alt="logo" />
+            <img src={Logo} alt="logo" className='logo' />
+
+            <ul className='header-menu'>
+                <li>Home</li>
+                <li>Programs</li>
+                <li>Why us</li>
+                <li>Plans</li>
+                <li>Testimonials</li>
+            </ul>
         </div>
     )
 }
