dcefde4d39bd42bab6633877c35a9f94be85f25c
Thomas Szentirmay <tlszent@outlook.com>
2023-08-21 18:45:17 +1000

    created text stroke font

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,10 +8,22 @@ const Hero = () => {
         <div className='hero'>
             <div className="left-h">
                 <Header />
-
+                
+                {/* The best ad */}
                 <div className="the-best-ad">
                     <div></div>
                     <span>The best fitness club in town</span>
+                </div>
+
+                {/* Hero Heading */}
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal Body</span>
+                    </div>
                 </div>
             </div>
             <div className="right-h">right side</div>
