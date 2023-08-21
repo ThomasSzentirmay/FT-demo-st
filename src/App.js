020392c47cc426a24635374e17c7c4d34c2beaae
Thomas Szentirmay <tlszent@outlook.com>
2023-08-21 20:16:19 +1000

    started building Programs component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 // Components
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 function App() {
   return (
     <div className="App">
           <Hero />
+          <Programs />
     </div>
   );
 }
