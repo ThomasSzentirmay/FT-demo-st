b567aa1c36884c5550a2b00fef6f94be58129104
Thomas Szentirmay <tlszent@outlook.com>
2023-08-21 18:28:48 +1000

    styling ad

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,12 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className="left-h">
-
                 <Header />
+
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>The best fitness club in town</span>
+                </div>
             </div>
             <div className="right-h">right side</div>
         </div>
