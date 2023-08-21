030a11e053828ad3add4a75413ea6a77818e72bf
Thomas Szentirmay <tlszent@outlook.com>
2023-08-21 19:42:43 +1000

    imported assets into hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Hero.css';
-
 import Header from '../Header/Header';
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
     return (
