bb4a6369b5cc8539ed32047be840d8680f216694
Thomas Szentirmay <tlszent@outlook.com>
2023-08-22 13:14:38 +1000

    useState for T data to clean things up

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Testimonials.css';
+import testimonialData from '../../data/testimonialsData';
 
 const Testimonials = () => {
+    const [selected, setSelected] = useState(0);
+    const tLength = testimonialData.length;
+
     return (
         <div className="Testimonials">
             <div className="left-t">
                 <span>Testimonials</span>
                 <span className='stroke-text'>What they</span>
                 <span>say about us</span>
+                <span>
+                    {testimonialData[selected].review}
+                </span>
             </div>
 
             <div className="right-t">
