e5fdfe5696997ece18d8995d99e941006ea44754
Thomas Szentirmay <tlszent@outlook.com>
2023-08-22 13:28:33 +1000

    styled 2 plain divs for background on T images

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Testimonials.css';
 import testimonialData from '../../data/testimonialsData';
+import leftArrow from '../../assets/leftArrow.png';
+import rightArrow from '../../assets/rightArrow.png';
 
 const Testimonials = () => {
     const [selected, setSelected] = useState(0);
@@ -15,10 +17,23 @@ const Testimonials = () => {
                 <span>
                     {testimonialData[selected].review}
                 </span>
+                <span>
+                    <span style={{color: 'var(--orange)'}}>
+                    {testimonialData[selected].name}
+                    </span>{""}
+                    - {testimonialData[selected].status}
+                </span>
             </div>
 
             <div className="right-t">
-                
+                <div></div>
+                <div></div>
+                <img src={testimonialData[selected].image} alt="" />
+
+                <div className="arrows">
+                    <img src={leftArrow} alt="" />
+                    <img src={rightArrow} alt="" />
+                </div>
             </div>
         </div>
     )
