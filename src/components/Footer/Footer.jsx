6d0dc095055e1e124e9f0a1f8b1bda1fdb0efc87
Thomas Szentirmay <tlszent@outlook.com>
2023-08-22 14:45:59 +1000

    set up footer structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Footer.css';
+import Github from '../../assets/github.png';
+import Instagram from '../../assets/Instagram.png';
+import LinkedIn from '../../assets/LinkedIn.png';
+import Logo from '../../assets/Logo.png';
 
 const Footer = () => {
     return (
-        <div className="Footer">
-
+        <div className="Footer-container">
+            <hr />
+            <div className="footer">
+                <img src={Github} alt="" />
+                <img src={Instagram} alt="" />
+                <img src={LinkedIn} alt="" />
+            </div>
+            <div className="logo-f">
+                <img src={Logo} alt="" />
+            </div>
         </div>
     )
 }
